16e65deba3a72838348cb6a6afc87a5044ca4857
NandhiniJothikrishh <126311373+NandhiniJothikrishh@users.noreply.github.com>
2023-10-12 10:27:41 +0530

    Add files via upload

---
+++ Backend/Day-19/src/main/java/com/nandha/meow/Repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.nandha.meow.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nandha.meow.Entity.ProductEntity;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ProductRepository extends
+JpaRepository<ProductEntity,Integer>,
+PagingAndSortingRepository<ProductEntity, Integer>{
+	
+}
